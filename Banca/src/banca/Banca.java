a37d3610d7bc79d7165240c0c2e57f7daeb0cb0a
ivan <costantinoivan94@gmail.com>
2018-03-26 17:35:56 +0200

    HO AGGIUNTO FINALMENTE IL MIO PEZZO.

--- Banca/src/banca/Banca.java
+++ Banca/src/banca/Banca.java
@@ -1,7 +1,34 @@
 package banca;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import contocorrente.ContoCorrente;
+
 public class Banca {
 	/**
 	 * gestire una lista di conto correnti
 	 */
+	ContoCorrente contoCorrente = new ContoCorrente();
+	private ArrayList<ContoCorrente> banca = new ArrayList<ContoCorrente>();
+	
+	public Banca () {
+		
+	}
+
+	public ArrayList<ContoCorrente> getBanca() {
+		ArrayList<ContoCorrente> stampaUtenti= null;
+		for(int x = 0 ;x <stampaUtenti.size();x++) {
+			x = stampaUtenti.get(x);
+		}
+		
+	}
+
+	public void setBanca(List<ContoCorrente> banca) {
+		this.banca = banca;
+	}
+	
+	
+	
 }

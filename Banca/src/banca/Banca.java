c9f38cc66fb24cf35d66d8f22f9adbd1e9739235
Dave <davebeltra92@gmail.com>
2018-03-27 16:35:19 +0200

    prototipo PreMain

--- Banca/src/banca/Banca.java
+++ Banca/src/banca/Banca.java
@@ -1,27 +1,19 @@
 package banca;
 
-
 import java.util.ArrayList;
 
 import contocorrente.ContoCorrente;
 
 public class Banca {
+	
 	/**
 	 * gestire una lista di conto correnti
 	 */
-	ContoCorrente contoCorrente = new ContoCorrente(null, null, 0);
+	
 	private ArrayList<ContoCorrente> listeContiCorrenti = new ArrayList<ContoCorrente>();
 	
 	public Banca () {
 		
-	}
-
-	public ContoCorrente getContoCorrente() {
-		return contoCorrente;
-	}
-
-	public void setContoCorrente(ContoCorrente contoCorrente) {
-		this.contoCorrente = contoCorrente;
 	}
 
 	public ArrayList<ContoCorrente> getListeContiCorrenti() {
@@ -33,25 +25,7 @@ public class Banca {
 	}
 
 	public String toString() {
-		return "Banca [contoCorrente=" + contoCorrente + ", listeContiCorrenti=" + listeContiCorrenti + "]";
-	}
-	
-	
-	
-
-	/*public ArrayList<ContoCorrente> getBanca() {
-		ArrayList<ContoCorrente> stampaUtenti= null;
-		for(int x = 0 ;x <stampaUtenti.size();x++) {
-			x = stampaUtenti.get();
-			System.out.println();
-		}
-		
-	}
-
-	public void setBanca(List<ContoCorrente> banca) {
-		this.banca = banca;
-	}
-	*/
-	
+		return "Banca [listeContiCorrenti=" + listeContiCorrenti + "]";
+	}	
 	
 }
